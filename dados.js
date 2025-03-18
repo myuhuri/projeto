e133a92ca3f05b6eb6da130f12e6962628c32256
myuhuri <emi.000013wx@gmail.com>
2025-03-18 12:18:01 -0300

    up

---
+++ dados.js
@@ -0,0 +1,16 @@
+let dados = [
+    {   nome : "Macbook",
+        descricao : "O MacBook, a renomada linha de computadores portáteis da Apple, personifica a convergência entre design elegante, desempenho excepcional e a perfeita integração com o ecossistema da Apple. Com dois modelos principais, o MacBook Air e o MacBook Pro, a Apple atende a uma ampla gama de necessidades e preferências.",
+        link : "https://www.apple.com/macbook"
+    },
+    {
+        nome : "iPhone",
+        descricao : "O iPhone, o smartphone da Apple, revolucionou a forma como nos comunicamos, trabalhamos e nos divertimos. Conhecido por seu design elegante, sistema operacional iOS intuitivo e câmeras de alta qualidade, o iPhone se tornou um símbolo de inovação e tecnologia.",
+        link : "https://www.apple.com/iphone"
+    },
+    {
+        nome : "Air Pods",
+        descricao : "Os AirPods, os fones de ouvido sem fio da Apple, revolucionaram a forma como as pessoas ouvem música, fazem chamadas e interagem com seus dispositivos. Conhecidos por seu design minimalista, qualidade de som excepcional e integração perfeita com o ecossistema da Apple, os AirPods se tornaram um acessório indispensável para muitos usuários.",
+        link : "https://www.apple.com/br/airpods/"
+    }
+];
