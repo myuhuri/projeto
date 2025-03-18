703a5efb9765b883241529275c3d872501a09988
myuhuri <emi.000013wx@gmail.com>
2025-03-18 12:19:45 -0300

    update

--- dados.js
+++ dados.js
@@ -1,16 +1,16 @@
 let dados = [
     {   nome : "Macbook",
-        descricao : "O MacBook, a renomada linha de computadores portáteis da Apple, personifica a convergência entre design elegante, desempenho excepcional e a perfeita integração com o ecossistema da Apple. Com dois modelos principais, o MacBook Air e o MacBook Pro, a Apple atende a uma ampla gama de necessidades e preferências.",
+        descricao : "O MacBook é uma linha de computadores portáteis desenvolvidos pela Apple, reconhecidos por seu design elegante, desempenho eficiente e recursos inovadores.",
         link : "https://www.apple.com/macbook"
     },
     {
         nome : "iPhone",
-        descricao : "O iPhone, o smartphone da Apple, revolucionou a forma como nos comunicamos, trabalhamos e nos divertimos. Conhecido por seu design elegante, sistema operacional iOS intuitivo e câmeras de alta qualidade, o iPhone se tornou um símbolo de inovação e tecnologia.",
+        descricao : "O iPhone é uma linha de smartphones da Apple, conhecida por sua alta qualidade, sistema operacional iOS exclusivo e recursos avançados de câmera e segurança.",
         link : "https://www.apple.com/iphone"
     },
     {
-        nome : "Air Pods",
-        descricao : "Os AirPods, os fones de ouvido sem fio da Apple, revolucionaram a forma como as pessoas ouvem música, fazem chamadas e interagem com seus dispositivos. Conhecidos por seu design minimalista, qualidade de som excepcional e integração perfeita com o ecossistema da Apple, os AirPods se tornaram um acessório indispensável para muitos usuários.",
-        link : "https://www.apple.com/br/airpods/"
+        nome : "Mac Air",
+        descricao : "O MacBook Air é um notebook ultraleve da Apple, conhecido por sua portabilidade, design fino e desempenho eficiente, ideal para tarefas do dia a dia e mobilidade.",
+        link : "https://www.apple.com/macbook-air"
     }
 ];
