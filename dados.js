d1c4a698e7c90504150a130de241fa2c4814abdc
thaemilly <129299585+myuhuri@users.noreply.github.com>
2025-03-25 12:19:23 -0300

    up

--- dados.js
+++ dados.js
@@ -1,16 +1,47 @@
 let dados = [
-    {   nome : "Macbook",
-        descricao : "O MacBook é uma linha de computadores portáteis desenvolvidos pela Apple, reconhecidos por seu design elegante, desempenho eficiente e recursos inovadores.",
-        link : "https://www.apple.com/macbook"
+    {
+        nome : "HomePod Mini",
+        descricao : "O HomePod Mini é um alto-falante inteligente compacto da Apple, oferecendo som surpreendente para seu tamanho e integração com a Siri.",
+        link : "https://www.apple.com/homepod-mini/"
     },
     {
-        nome : "iPhone",
-        descricao : "O iPhone é uma linha de smartphones da Apple, conhecida por sua alta qualidade, sistema operacional iOS exclusivo e recursos avançados de câmera e segurança.",
-        link : "https://www.apple.com/iphone"
+        nome : "iPad Pro",
+        descricao : "O iPad Pro oferece desempenho poderoso com o chip M2, tela Liquid Retina e suporte ao Apple Pencil, perfeito para profissionais e criativos.",
+        link : "https://www.apple.com/ipad-pro/"
     },
     {
-        nome : "Mac Air",
-        descricao : "O MacBook Air é um notebook ultraleve da Apple, conhecido por sua portabilidade, design fino e desempenho eficiente, ideal para tarefas do dia a dia e mobilidade.",
-        link : "https://www.apple.com/macbook-air"
+        nome : "Iphone",
+        descricao : "O Magic Keyboard proporciona uma experiência de digitação excepcional para iPad e Mac, com design elegante e conforto incomparável.",
+        link : "https://www.apple.com/br/shop/buy-iphone"
+    },
+    {
+        nome : "Apple Pencil",
+        descricao : "O iPhone é um smartphone da Apple que se destaca por sua combinação de design elegante, tecnologia avançada e um ecossistema integrado. .",
+        link : "https://www.apple.com/apple-pencil/"
+    },
+    {
+        nome : "iMac",
+        descricao : "O iMac é um desktop elegante e potente, com tela Retina deslumbrante e desempenho excepcional para trabalho e lazer.",
+        link : "https://www.apple.com/imac"
+    },
+    {
+        nome : "Mac Studio",
+        descricao : "O Mac Studio é um computador potente projetado para profissionais criativos, com desempenho incrível e versatilidade.",
+        link : "https://www.apple.com/mac-studio/"
+    },
+    {
+        nome : "AirTag",
+        descricao : "O AirTag ajuda a localizar itens pessoais com facilidade, integrando-se ao aplicativo Buscar para rastreamento em tempo real.",
+        link : "https://www.apple.com/airtag/"
+    },
+    {
+        nome : "Apple Arcade",
+        descricao : "O Apple Arcade oferece acesso ilimitado a uma vasta biblioteca de jogos exclusivos para dispositivos Apple.",
+        link : "https://www.apple.com/apple-arcade/"
+    },
+    {
+        nome : "Apple One",
+        descricao : "O Apple One combina vários serviços da Apple em uma única assinatura conveniente e acessível.",
+        link : "https://www.apple.com/apple-one/"
     }
 ];
--- script.js
+++ script.js
@@ -5,31 +5,28 @@ function pesquisar(){
     campoPesquisa = campoPesquisa.toLowerCase();
 
     if (campoPesquisa == ""){
-        section.innerHTML = "Nenhum texto digitado!";
+        section.innerHTML = "<p>Nenhum texto digitado!</p>";
         return;
     }
 
     let resultados = "";
-    let nome = "";
-    let descricao = "";
     for (let dado of dados){
-        nome = dado.nome.toLocaleLowerCase();
-        descricao = dado.descricao.toLocaleLowerCase();
+        let nome = dado.nome.toLowerCase();
+        let descricao = dado.descricao.toLowerCase();
         
         if (nome.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
-            resultados += 
-        `
-            <div class="item-resultado">
-                <h2><a href="#" target="_blank">${dado.nome}</a></h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <a href="${dado.link}" target="_blank" rel="noopener noreferrer">Mais Informações</a>
-            </div>
-        `
+            resultados += `
+                <div class="item-resultado">
+                    <h2><a href="${dado.link}" target="_blank">${dado.nome}</a></h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href="${dado.link}" target="_blank" rel="noopener noreferrer">Mais Informações</a>
+                </div>
+            `;
         }
-        
     }
+
     if(!resultados) {
-        resultados = "<p>Resultado não Encontrado</p>"
+        resultados = "<p>Resultado não Encontrado</p>";
     }     
     section.innerHTML = resultados;
 }
